3023a066ca830a1c14f53b57d5fc2ac52b91521d
Joseph Cannova <jcannova@wheelership.com>
2019-05-03 18:06:31 -0400

    Fixed issues with handlebars logic in the favotires section

--- routes/htmlRoutes/profile.js
+++ routes/htmlRoutes/profile.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('parent', { title: 'Manage Profile' });
+    res.render('parent', { favorites: '' });
   });
 
   module.exports = router;
