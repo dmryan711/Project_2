92d79bcf6214ee224a7f66ed64cd5433444ed99e
Pickle Rick <dmryan711@gmail.com>
2019-05-02 21:32:46 -0400

    Sign Up and Sign Up failure created with test hbs files for sign up

---
+++ routes/htmlRoutes/signUp.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+
+
+router.get("/",function(req,res){
+    res.render('signUp', { title: 'Sign Up for School Finder' });
+});
+
+router.post('/', passport.authenticate('local-signup', {
+    successRedirect: '/',
+    failureRedirect: '/signupfailed'
+}
+
+));
+
+module.exports = router;
